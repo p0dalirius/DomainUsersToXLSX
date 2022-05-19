4898cbc09a29e2c2a03649275377606376e796ae
p0dalirius <podalirius@protonmail.com>
2022-05-19 13:34:20 +0200

    Public release

---
+++ DomainUsersToXLSX.py
@@ -0,0 +1,400 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File name          : DomainUsersToXLSX.py
+# Author             : Podalirius (@podalirius_)
+# Date created       : 19 May 2022
+
+import datetime
+import xlsxwriter
+from impacket.examples import logger, utils
+from impacket import version
+from impacket.smbconnection import SMBConnection, SMB2_DIALECT_002, SMB2_DIALECT_21, SMB_DIALECT, SessionError
+from impacket.spnego import SPNEGO_NegTokenInit, TypesMech
+import argparse
+import binascii
+import ldap3
+import logging
+import os
+import ssl
+import sys
+
+
+def get_domain_users(ldap_server, ldap_session, attrs=["*"]):
+    ts_never_low  = datetime.datetime(1601,  1,  1,  0,  0, tzinfo=datetime.timezone.utc)
+    ts_never_high = datetime.datetime(9999, 12, 31, 23, 59, 59, 999999, tzinfo=datetime.timezone.utc)
+    results = {}
+    target_dn = ldap_server.info.other["defaultNamingContext"]
+    ldap_session.search(target_dn, "(objectCategory=person)", attributes=attrs)
+    for entry in ldap_session.response:
+        if entry['type'] != 'searchResEntry':
+            continue
+        results[entry['dn']] = {}
+        for attrname in attrs:
+            if attrname in ['name', 'sAMAccountName', 'distinguishedName', 'logonCount']:
+                results[entry['dn']][attrname] = entry["attributes"][attrname]
+
+            elif attrname in ['memberOf', 'description']:
+                results[entry['dn']][attrname] = '\n'.join(entry["attributes"][attrname])
+
+            elif attrname in ['adminCount']:
+                results[entry['dn']][attrname] = entry["attributes"][attrname]
+                if (type(entry["attributes"][attrname]) == list):
+                    if (len(entry["attributes"][attrname]) == 0):
+                        results[entry['dn']][attrname] = 0
+
+            elif attrname in ['whenCreated', 'pwdLastSet', 'lastLogon', 'lastLogoff', 'lastLogonTimestamp', 'accountExpires']:
+                if (type(entry["attributes"][attrname]) == list):
+                    if (len(entry["attributes"][attrname]) == 0):
+                        results[entry['dn']][attrname] = "Never"
+                elif (entry["attributes"][attrname] == ts_never_low) or (entry["attributes"][attrname] ==  ts_never_high):
+                    results[entry['dn']][attrname] = "Never"
+                else:
+                    results[entry['dn']][attrname] = entry["attributes"][attrname].strftime("%m/%d/%Y, %H:%M:%S")
+
+    return results
+
+
+def parse_args():
+    print("DomainUsersToXLSX v1.2 - by @podalirius_\n")
+
+    parser = argparse.ArgumentParser(add_help=True, description='Find uncommon SMB shares on remote machines.')
+    parser.add_argument('-ts', action='store_true', help='Adds timestamp to every logging output')
+    parser.add_argument('--use-ldaps', action='store_true', help='Use LDAPS instead of LDAP')
+    parser.add_argument("-q", "--quiet", dest="quiet", action="store_true", default=False, help="Show no information at all.")
+    parser.add_argument("-debug", dest="debug", action="store_true", default=False, help="Debug mode.")
+    parser.add_argument("-no-colors", dest="colors", action="store_false", default=True, help="Disables colored output mode")
+    parser.add_argument("-I", "--ignore-hidden-shares", dest="ignore_hidden_shares", action="store_true", default=False, help="Ignores hidden shares (shares ending with $)")
+    parser.add_argument("-t", "--threads", dest="threads", action="store", type=int, default=20, required=False, help="Number of threads (default: 20)")
+    parser.add_argument("-o", "--output-file", dest="output_file", type=str, default="accounts.xlsx", required=False, help="Output file to store the results in. (default: shares.json)")
+
+    authconn = parser.add_argument_group('authentication & connection')
+    authconn.add_argument('--dc-ip', required=True, action='store', metavar="ip address", help='IP Address of the domain controller or KDC (Key Distribution Center) for Kerberos. If omitted it will use the domain part (FQDN) specified in the identity parameter')
+    authconn.add_argument("-d", "--domain", dest="auth_domain", metavar="DOMAIN", action="store", help="(FQDN) domain to authenticate to")
+    authconn.add_argument("-u", "--user", dest="auth_username", metavar="USER", action="store", help="user to authenticate with")
+
+    secret = parser.add_argument_group()
+    cred = secret.add_mutually_exclusive_group()
+    cred.add_argument("--no-pass", action="store_true", help="Don't ask for password (useful for -k)")
+    cred.add_argument("-p", "--password", dest="auth_password", metavar="PASSWORD", action="store", help="Password to authenticate with")
+    cred.add_argument("-H", "--hashes", dest="auth_hashes", action="store", metavar="[LMHASH:]NTHASH", help='NT/LM hashes, format is LMhash:NThash')
+    cred.add_argument("--aes-key", dest="auth_key", action="store", metavar="hex key", help='AES key to use for Kerberos Authentication (128 or 256 bits)')
+    secret.add_argument("-k", "--kerberos", dest="use_kerberos", action="store_true", help='Use Kerberos authentication. Grabs credentials from .ccache file (KRB5CCNAME) based on target parameters. If valid credentials cannot be found, it will use the ones specified in the command line')
+
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+
+    args = parser.parse_args()
+
+    return args
+
+
+def get_machine_name(args, domain):
+    if args.dc_ip is not None:
+        s = SMBConnection(args.dc_ip, args.dc_ip)
+    else:
+        s = SMBConnection(domain, domain)
+    try:
+        s.login('', '')
+    except Exception:
+        if s.getServerName() == '':
+            raise Exception('Error while anonymous logging into %s' % domain)
+    else:
+        s.logoff()
+    return s.getServerName()
+
+
+def init_ldap_connection(target, tls_version, args, domain, username, password, lmhash, nthash):
+    user = '%s\\%s' % (domain, username)
+    if tls_version is not None:
+        use_ssl = True
+        port = 636
+        tls = ldap3.Tls(validate=ssl.CERT_NONE, version=tls_version)
+    else:
+        use_ssl = False
+        port = 389
+        tls = None
+    ldap_server = ldap3.Server(target, get_info=ldap3.ALL, port=port, use_ssl=use_ssl, tls=tls)
+
+    if args.use_kerberos:
+        ldap_session = ldap3.Connection(ldap_server)
+        ldap_session.bind()
+        ldap3_kerberos_login(ldap_session, target, username, password, domain, lmhash, nthash, args.auth_key, kdcHost=args.dc_ip)
+    elif args.auth_hashes is not None:
+        if lmhash == "":
+            lmhash = "aad3b435b51404eeaad3b435b51404ee"
+        ldap_session = ldap3.Connection(ldap_server, user=user, password=lmhash + ":" + nthash, authentication=ldap3.NTLM, auto_bind=True)
+    else:
+        ldap_session = ldap3.Connection(ldap_server, user=user, password=password, authentication=ldap3.NTLM, auto_bind=True)
+
+    return ldap_server, ldap_session
+
+
+def init_ldap_session(args, domain, username, password, lmhash, nthash):
+    if args.use_kerberos:
+        target = get_machine_name(args, domain)
+    else:
+        if args.dc_ip is not None:
+            target = args.dc_ip
+        else:
+            target = domain
+
+    if args.use_ldaps is True:
+        try:
+            return init_ldap_connection(target, ssl.PROTOCOL_TLSv1_2, args, domain, username, password, lmhash, nthash)
+        except ldap3.core.exceptions.LDAPSocketOpenError:
+            return init_ldap_connection(target, ssl.PROTOCOL_TLSv1, args, domain, username, password, lmhash, nthash)
+    else:
+        return init_ldap_connection(target, None, args, domain, username, password, lmhash, nthash)
+
+
+def ldap3_kerberos_login(connection, target, user, password, domain='', lmhash='', nthash='', aesKey='', kdcHost=None, TGT=None, TGS=None, useCache=True):
+    from pyasn1.codec.ber import encoder, decoder
+    from pyasn1.type.univ import noValue
+    """
+    logins into the target system explicitly using Kerberos. Hashes are used if RC4_HMAC is supported.
+    :param string user: username
+    :param string password: password for the user
+    :param string domain: domain where the account is valid for (required)
+    :param string lmhash: LMHASH used to authenticate using hashes (password is not used)
+    :param string nthash: NTHASH used to authenticate using hashes (password is not used)
+    :param string aesKey: aes256-cts-hmac-sha1-96 or aes128-cts-hmac-sha1-96 used for Kerberos authentication
+    :param string kdcHost: hostname or IP Address for the KDC. If None, the domain will be used (it needs to resolve tho)
+    :param struct TGT: If there's a TGT available, send the structure here and it will be used
+    :param struct TGS: same for TGS. See smb3.py for the format
+    :param bool useCache: whether or not we should use the ccache for credentials lookup. If TGT or TGS are specified this is False
+    :return: True, raises an Exception if error.
+    """
+
+    if lmhash != '' or nthash != '':
+        if len(lmhash) % 2:
+            lmhash = '0' + lmhash
+        if len(nthash) % 2:
+            nthash = '0' + nthash
+        try:  # just in case they were converted already
+            lmhash = binascii.unhexlify(lmhash)
+            nthash = binascii.unhexlify(nthash)
+        except TypeError:
+            pass
+
+    # Importing down here so pyasn1 is not required if kerberos is not used.
+    from impacket.krb5.ccache import CCache
+    from impacket.krb5.asn1 import AP_REQ, Authenticator, TGS_REP, seq_set
+    from impacket.krb5.kerberosv5 import getKerberosTGT, getKerberosTGS
+    from impacket.krb5 import constants
+    from impacket.krb5.types import Principal, KerberosTime, Ticket
+    import datetime
+
+    if TGT is not None or TGS is not None:
+        useCache = False
+
+    if useCache:
+        try:
+            ccache = CCache.loadFile(os.getenv('KRB5CCNAME'))
+        except Exception as e:
+            # No cache present
+            print(e)
+            pass
+        else:
+            # retrieve domain information from CCache file if needed
+            if domain == '':
+                domain = ccache.principal.realm['data'].decode('utf-8')
+                logging.debug('Domain retrieved from CCache: %s' % domain)
+
+            logging.debug('Using Kerberos Cache: %s' % os.getenv('KRB5CCNAME'))
+            principal = 'ldap/%s@%s' % (target.upper(), domain.upper())
+
+            creds = ccache.getCredential(principal)
+            if creds is None:
+                # Let's try for the TGT and go from there
+                principal = 'krbtgt/%s@%s' % (domain.upper(), domain.upper())
+                creds = ccache.getCredential(principal)
+                if creds is not None:
+                    TGT = creds.toTGT()
+                    logging.debug('Using TGT from cache')
+                else:
+                    logging.debug('No valid credentials found in cache')
+            else:
+                TGS = creds.toTGS(principal)
+                logging.debug('Using TGS from cache')
+
+            # retrieve user information from CCache file if needed
+            if user == '' and creds is not None:
+                user = creds['client'].prettyPrint().split(b'@')[0].decode('utf-8')
+                logging.debug('Username retrieved from CCache: %s' % user)
+            elif user == '' and len(ccache.principal.components) > 0:
+                user = ccache.principal.components[0]['data'].decode('utf-8')
+                logging.debug('Username retrieved from CCache: %s' % user)
+
+    # First of all, we need to get a TGT for the user
+    userName = Principal(user, type=constants.PrincipalNameType.NT_PRINCIPAL.value)
+    if TGT is None:
+        if TGS is None:
+            tgt, cipher, oldSessionKey, sessionKey = getKerberosTGT(userName, password, domain, lmhash, nthash, aesKey, kdcHost)
+    else:
+        tgt = TGT['KDC_REP']
+        cipher = TGT['cipher']
+        sessionKey = TGT['sessionKey']
+
+    if TGS is None:
+        serverName = Principal('ldap/%s' % target, type=constants.PrincipalNameType.NT_SRV_INST.value)
+        tgs, cipher, oldSessionKey, sessionKey = getKerberosTGS(serverName, domain, kdcHost, tgt, cipher, sessionKey)
+    else:
+        tgs = TGS['KDC_REP']
+        cipher = TGS['cipher']
+        sessionKey = TGS['sessionKey']
+
+        # Let's build a NegTokenInit with a Kerberos REQ_AP
+
+    blob = SPNEGO_NegTokenInit()
+
+    # Kerberos
+    blob['MechTypes'] = [TypesMech['MS KRB5 - Microsoft Kerberos 5']]
+
+    # Let's extract the ticket from the TGS
+    tgs = decoder.decode(tgs, asn1Spec=TGS_REP())[0]
+    ticket = Ticket()
+    ticket.from_asn1(tgs['ticket'])
+
+    # Now let's build the AP_REQ
+    apReq = AP_REQ()
+    apReq['pvno'] = 5
+    apReq['msg-type'] = int(constants.ApplicationTagNumbers.AP_REQ.value)
+
+    opts = []
+    apReq['ap-options'] = constants.encodeFlags(opts)
+    seq_set(apReq, 'ticket', ticket.to_asn1)
+
+    authenticator = Authenticator()
+    authenticator['authenticator-vno'] = 5
+    authenticator['crealm'] = domain
+    seq_set(authenticator, 'cname', userName.components_to_asn1)
+    now = datetime.datetime.utcnow()
+
+    authenticator['cusec'] = now.microsecond
+    authenticator['ctime'] = KerberosTime.to_asn1(now)
+
+    encodedAuthenticator = encoder.encode(authenticator)
+
+    # Key Usage 11
+    # AP-REQ Authenticator (includes application authenticator
+    # subkey), encrypted with the application session key
+    # (Section 5.5.1)
+    encryptedEncodedAuthenticator = cipher.encrypt(sessionKey, 11, encodedAuthenticator, None)
+
+    apReq['authenticator'] = noValue
+    apReq['authenticator']['etype'] = cipher.enctype
+    apReq['authenticator']['cipher'] = encryptedEncodedAuthenticator
+
+    blob['MechToken'] = encoder.encode(apReq)
+
+    request = ldap3.operation.bind.bind_operation(connection.version, ldap3.SASL, user, None, 'GSS-SPNEGO',
+                                                  blob.getData())
+
+    # Done with the Kerberos saga, now let's get into LDAP
+    if connection.closed:  # try to open connection if closed
+        connection.open(read_server_info=False)
+
+    connection.sasl_in_progress = True
+    response = connection.post_send_single_response(connection.send('bindRequest', request, None))
+    connection.sasl_in_progress = False
+    if response[0]['result'] != 0:
+        raise Exception(response)
+
+    connection.bound = True
+
+    return True
+
+
+def init_logger(args):
+    # Init the example's logger theme and debug level
+    logger.init(args.ts)
+    if args.debug is True:
+        logging.getLogger().setLevel(logging.DEBUG)
+        # Print the Library's installation path
+        logging.debug(version.getInstallationPath())
+    else:
+        logging.getLogger().setLevel(logging.INFO)
+        logging.getLogger('impacket.smbserver').setLevel(logging.ERROR)
+
+
+def init_smb_session(args, target_ip, domain, username, password, address, lmhash, nthash, port=445):
+    smbClient = SMBConnection(address, target_ip, sess_port=int(port))
+    dialect = smbClient.getDialect()
+    if dialect == SMB_DIALECT:
+        logging.debug("SMBv1 dialect used")
+    elif dialect == SMB2_DIALECT_002:
+        logging.debug("SMBv2.0 dialect used")
+    elif dialect == SMB2_DIALECT_21:
+        logging.debug("SMBv2.1 dialect used")
+    else:
+        logging.debug("SMBv3.0 dialect used")
+    if args.use_kerberos is True:
+        smbClient.kerberosLogin(username, password, domain, lmhash, nthash, args.aesKey, args.dc_ip)
+    else:
+        smbClient.login(username, password, domain, lmhash, nthash)
+    if smbClient.isGuestSession() > 0:
+        logging.debug("GUEST Session Granted")
+    else:
+        logging.debug("USER Session Granted")
+    return smbClient
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    init_logger(args)
+
+    auth_lm_hash = ""
+    auth_nt_hash = ""
+    if args.auth_hashes is not None:
+        if ":" in args.auth_hashes:
+            auth_lm_hash = args.auth_hashes.split(":")[0]
+            auth_nt_hash = args.auth_hashes.split(":")[1]
+        else:
+            auth_nt_hash = args.auth_hashes
+
+    ldap_server, ldap_session = init_ldap_session(
+        args=args,
+        domain=args.auth_domain,
+        username=args.auth_username,
+        password=args.auth_password,
+        lmhash=auth_lm_hash,
+        nthash=auth_nt_hash
+    )
+
+    interesting_attributes = [
+        'name', 'sAMAccountName', 'distinguishedName', 'description',
+        'memberOf',
+        'pwdLastSet', 'whenCreated', 'lastLogon', 'logonCount',  'lastLogonTimestamp', 'lastLogoff',
+        'adminCount', 'accountExpires'
+    ]
+
+    if not args.quiet:
+        print("[>] Extracting all users ...")
+    users = get_domain_users(ldap_server, ldap_session, attrs=interesting_attributes)
+
+    if not args.quiet:
+        print("[+] Found %d users in the domain." % len(users.keys()))
+        print()
+
+    workbook = xlsxwriter.Workbook(args.output_file)
+    worksheet = workbook.add_worksheet()
+
+    header_format = workbook.add_format({'bold': 1})
+
+    for k in range(len(interesting_attributes)):
+        worksheet.set_column(k, k+1, len(interesting_attributes[k])+3)
+
+    worksheet.set_row(0, 20, header_format)
+    worksheet.write_row(0, 0, interesting_attributes)
+
+    row_id = 1
+    for dn, user in users.items():
+        if not args.quiet:
+            print("   [>] Adding %s ..." % dn)
+        data = [user.get(f, "") for f in interesting_attributes]
+        worksheet.write_row(row_id, 0, data)
+        row_id += 1
+
+    workbook.close()
+    print("[+] List of users written to %s" % args.output_file)
--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # DomainUsersToXLSX
-Extract all users from an Active Directory domain to an Excel worksheet.
+
+<p align="center">
+  This Python script can be used to bypass IP source restrictions using HTTP headers.
+  <br>
+  <img src="https://badges.pufler.dev/visits/p0dalirius/DomainUsersToXLSX/"/>
+  <img alt="GitHub release (latest by date)" src="https://img.shields.io/github/v/release/p0dalirius/DomainUsersToXLSX">
+  <a href="https://twitter.com/intent/follow?screen_name=podalirius_" title="Follow"><img src="https://img.shields.io/twitter/follow/podalirius_?label=Podalirius&style=social"></a>
+  <br>
+</p>
+
+## Features
+
+ - [x] Extract all domain users to Excel worksheet
+
+## Usage
+
+```
+$ ./ipsourcebypass.py -h
+[~] IP source bypass using HTTP headers, v1.2
+
+usage: ipsourcebypass.py [-h] [-v] -i IP [-t THREADS] [-x PROXY] [-k] [-L] [-j JSONFILE] [-C] [-H HEADERS] [-S] url
+
+This Python script can be used to test for IP source bypass using HTTP headers
+
+positional arguments:
+  url                   e.g. https://example.com:port/path
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -v, --verbose         arg1 help message
+  -i IP, --ip IP        IP to spoof.
+  -t THREADS, --threads THREADS
+                        Number of threads (default: 5)
+  -x PROXY, --proxy PROXY
+                        Specify a proxy to use for requests (e.g., http://localhost:8080)
+  -k, --insecure        Allow insecure server connections when using SSL (default: False)
+  -L, --location        Follow redirects (default: False)
+  -j JSONFILE, --jsonfile JSONFILE
+                        Save results to specified JSON file.
+  -C, --curl            Generate curl commands for each request.
+  -H HEADERS, --header HEADERS
+                        arg1 help message
+  -S, --save            Save all HTML responses.
+```
+
+## Demonstration
+
+![](./.github/extract.png)
+
+![](./.github/excel.png)
+
+## Contributing
+
+Pull requests are welcome. Feel free to open an issue if you want to add other features.
